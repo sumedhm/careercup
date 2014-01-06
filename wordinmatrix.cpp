fb7c9ab08f51af5157aeb85836b606675f7571c3
sumedhm <sumedhm18@gmail.com>
2014-01-06 04:52:42 -0800

    Search for occurences of word in matrix

--- wordinmatrix.cpp
+++ wordinmatrix.cpp
@@ -3,41 +3,61 @@
 */
 
 #include <iostream>
+#include <vector>
+#include <stdlib.h>
 using namespace std;
 
 int n;
 
-int check_horizontally(char string[], char matrix[n][], int i, int j){
-	int match = 1, k = 0;
-	while(j<n && string[k]!='\0'){
+int check_horizontally(char string[], char **matrix, int i, int j){
+	int match = 0, k = 0;
+	//cout << "\nChecking horizontally @ " << i << "," << j << ", found : ";
+	while(j<n){
 		if(matrix[i][j] != string[k]){
-			match = 0; j++; k++;
+			match = 0; break;
+		}
+		j++; k++;
+		if(string[k]=='\0'){
+			match = 1; break;
 		}
 	}
+	//cout << match <".\n";
 	return match;
 }
 
-int check_vertically(char string[], char matrix[][], int i, int j, int n){
-        int match = 1, k = 0;
-        while(i<n && string[k]!='\0'){
+int check_vertically(char string[], char **matrix, int i, int j){
+        int match = 0, k = 0;
+	//cout << "\nChecking vertically @ " << i << "," << j << ", found : ";
+        while(i<n){
                 if(matrix[i][j] != string[k]){
-                        match = 0; i++; k++;
+                        match = 0; break;
+                }
+		i++; k++;
+		if(string[k]=='\0'){
+                        match = 1; break;
                 }
         }
+	//cout << match <".\n";
         return match;
 }
 
-int check_diagonally(char string[], char matrix[][], int i, int j, int n){
-        int match = 1, k = 0;
-        while(j<n && i<n && string[k]!='\0'){
+int check_diagonally(char string[], char **matrix, int i, int j){
+        int match = 0, k = 0;
+	//cout << "\nChecking diagonally @ " << i << "," << j << ", found : ";
+        while(j<n && i<n){
                 if(matrix[i][j] != string[k]){
-                        match = 0; i++; j++; k++;
+                        match = 0; break;
+                }
+		i++; j++; k++;
+		if(string[k]=='\0'){
+                        match = 1; break;
                 }
         }
+	//cout << match <".\n";
         return match;
 }
 
-int checkfor(char string[], char matrix[][], int i, int j, int n){
+int checkfor(char string[], char **matrix, int i, int j){
 	return (check_horizontally(string, matrix, i, j) + 
 		check_vertically(string, matrix, i, j) + 
 		check_diagonally(string, matrix, i, j));
@@ -48,14 +68,18 @@ int main(){
 	int occurences=0;
 	cout << "\nEnter the value of n: ";
 	cin >> n;
-	char matrix[n][n];
+	char **matrix;
 	cout << "\n\nInput the matrix:\n";
-	for(i=0;i<n;i++) for(j=0;j<n;j++) cin >> matrix[i][j];
+	matrix = (char **) malloc (sizeof(char *) * i);
+	for(i=0;i<n;i++){
+		matrix[i] = (char *) malloc (sizeof(char) * j);
+		for(j=0;j<n;j++) cin >> matrix[i][j];
+	}
 	char string[n];
 	cout << "\n\nInput the string(<=n): ";
 	cin >> string; cout<< "\n";
-	for(i=0;i<n;i++) for(j=0;j<n;j++) if(string[0]==matrix[i][j]) occurences += checkfor(string, matrix, i, j, n);
-	cout << "\n\nNumber of occurences of " << string << "in the matrix are " << occurences << ".\n\n";
+	for(i=0;i<n;i++) for(j=0;j<n;j++) if(string[0]==matrix[i][j]) occurences += checkfor(string, matrix, i, j);
+	cout << "\n\nNumber of occurences of " << string << " in the matrix are " << occurences << ".\n\n";
 
 	return 0;
 }
